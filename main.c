e32af01243c5fa5d81c968f784da20d52b2f2dfc
Trond Albinussen <trond.albinussen@icloud.com>
2025-10-28 22:47:53 +0100

    function by ref

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void swap(int *x, int *y);
+
 int main() {
 
     printf("__STDC_VERSION__: %ld\n", __STDC_VERSION__);
@@ -11,5 +13,25 @@ int main() {
     printf("Address of x is: %u\n", x);
     printf("Address of p is: %p\n", p);
 
+    int a = 5;
+    int b = 10;
+
+    printf("a is %d\n", a);
+    printf("b is %d\n", b);
+
+    // pass int the addresses of a and b
+    swap(&a, &b);
+
+    printf("a is now %d\n", a);
+    printf("b is now %d\n", b);
+
     return 0;
+}
+
+// function return nothing, just swap in mem. // pass by ref?
+void swap(int *a, int *b) {
+    int temp = 0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
